26924f27cb474cfabe07f272ea15f276684dbe8c
sabaoth <sabaoth@ukr.net>
2021-01-21 15:19:07 +0200

    delete

--- mean-app/backend/app.js
+++ mean-app/backend/app.js
@@ -36,15 +36,12 @@ app.post("/api/posts", (req, res, next) => {
   const post = new Post({ title: req.body.title, content: req.body.content });
   console.log(post);
 
-  post.save();
-  res.status(201).json({
-    message: "Post added successfully",
+  post.save().then((result) => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: result._id,
+    });
   });
-});
-
-app.use((req, res, next) => {
-  console.log("first Middleware");
-  next();
 });
 
 app.get("/api/posts", (req, res, next) => {
@@ -56,6 +53,13 @@ app.get("/api/posts", (req, res, next) => {
       });
     })
     .catch((err) => console.log(err));
+});
+
+app.delete("/api/posts:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted" });
+  });
 });
 
 module.exports = app;
--- mean-app/src/app/posts/post-create/posts.service.ts
+++ mean-app/src/app/posts/post-create/posts.service.ts
@@ -2,23 +2,47 @@ import { Post } from './post.model';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
-  private posts: Post[] = [];
+  posts: any = [];
   private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     this.http
-      .get<{ message: String; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: String; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          let data = postData.posts.map((post) => {
+            return { title: post.title, content: post.content, id: post._id };
+          });
+          return data;
+        })
       )
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .subscribe((data) => {
+        this.posts = data;
         this.postsUpdated.next([...this.posts]);
       });
   }
 
+  /*   getPosts() {
+    this.http
+      .get<{ message: String; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post) => {
+            return { title: post.title, content: post.content, id: post._id };
+          });
+        })
+      )
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts.posts;
+        console.log(this.posts);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+ */
   getPostsUpdateListener() {
     return this.postsUpdated.asObservable();
   }
@@ -26,10 +50,27 @@ export class PostsService {
   addPosts(title: string, contant: string) {
     const post: Post = { title: title, content: contant };
     this.http
-      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .post<{ message: string; postId: string }>(
+        'http://localhost:3000/api/posts',
+        post
+      )
       .subscribe((response) => {
-        console.log(response);
+        const postId = response.postId;
+        post.id = postId;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http
+      .delete('http://localhost:3000/api/posts' + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter((post) => {
+          return post.id !== postId;
+        });
+        console.log(updatedPosts);
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
